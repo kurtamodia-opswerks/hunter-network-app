e887ea234bd7c1f62b09dc1e0ccb9b8558cb20d3
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-08-29 22:04:34 +0800

    feat: add LoginForm

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,44 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+export default function LoginForm() {
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your credentials to access your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="Enter your email" />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <CardAction>
+          <Button className="w-full">Login</Button>
+        </CardAction>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,11 @@
+import LoginForm from "@/components/LoginForm";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <div classname="home-container w-full h-screen flex justify-center items-center">
+        <LoginForm />
+      </div>
+    </>
+  );
 }
